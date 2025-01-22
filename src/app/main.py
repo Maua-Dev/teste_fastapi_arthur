db64a6752c2afd8e3953ab58705f5ee983e43097
Arthur Silva <xarthsilvax@gmail.com>
2025-01-22 13:57:39 -0300

    print em get_all_items

--- src/app/main.py
+++ src/app/main.py
@@ -18,6 +18,7 @@ repo = Environments.get_item_repo()()
 
 @app.get("/items/get_all_items")
 def get_all_items():
+    print("Entrando no get_all_items")
     items = repo.get_all_items()
     
     items_list = list()
